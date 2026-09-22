46319bc744722ed9a8e16a7515a04685ac25b55a
Schoenfeld, Max <Max.Schoenfeld@surescripts.com>
2026-09-22 15:57:42 -0400

    add tasker user to the docker group

--- setup.sh
+++ setup.sh
@@ -3,3 +3,5 @@ sudo dnf config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashi
 sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
 sudo dnf install -y terraform docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo systemctl enable --now docker
+sudo groupadd -f docker
+sudo usermod -aG docker tasker
