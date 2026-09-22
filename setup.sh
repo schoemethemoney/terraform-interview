b2bb3fa87cd460a685e205e42c22f39193548489
Schoenfeld, Max <Max.Schoenfeld@surescripts.com>
2026-09-22 15:20:01 -0400

    adds update setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
-sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
 sudo yum -y install terraform
+dnf install -y podman
+dnf install -y podman-docker
